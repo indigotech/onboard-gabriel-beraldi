84beb0a5c853b186e5a145b54f0908298a9cf98e
BaraldsBR <gabriel.baraldi@taqtile.com.br>
2025-01-08 17:05:43 -0300

    add input validation

--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -3,6 +3,63 @@ import * as React from "react";
 import { Pressable, Text, View } from "react-native";
 
 export default function Index() {
+  const [email, setEmail] = React.useState("");
+
+  function handleEmailChange(newEmail: string) {
+    setEmail(newEmail);
+  }
+
+  const [password, setPassword] = React.useState("");
+
+  function handlePasswordChange(newPassword: string) {
+    setPassword(newPassword);
+  }
+
+  function handleSubmit() {
+    if (!email) {
+      console.log("empty email");
+      return;
+    }
+
+    const [local, domain, ...invalidChars] = email.split("@");
+    if (invalidChars && invalidChars.length > 0) {
+      console.log("invalid email");
+      return;
+    }
+
+    if (!local || local.length === 0) {
+      console.log("invalid email");
+      return;
+    }
+
+    if (!domain || domain.length <= 4) {
+      console.log("invalid email");
+      return;
+    }
+
+    const lastHostname = domain.split(".").pop();
+
+    if (!lastHostname || lastHostname !== "com") {
+      console.log("invalid email");
+      return;
+    }
+
+    if (!password) {
+      console.log("empty password");
+      return;
+    }
+
+    if (password.length < 7) {
+      console.log("password must be at least 7 characters long");
+      return;
+    }
+
+    if (!password.match(/^(?=.*[A-Za-z])(?=.*\d).*$/gm)) {
+      console.log("password must contain both a letter and a number");
+      return;
+    }
+  }
+
   return (
     <View
       style={{
@@ -11,9 +68,18 @@ export default function Index() {
         gap: 8,
       }}
     >
-      <LabeledField label="Email:" />
-      <LabeledField label="Senha:" />
+      <LabeledField
+        label="Email:"
+        value={email}
+        onValueChange={handleEmailChange}
+      />
+      <LabeledField
+        label="Senha:"
+        value={password}
+        onValueChange={handlePasswordChange}
+      />
       <Pressable
+        onPress={handleSubmit}
         style={{
           justifyContent: "center",
           alignItems: "center",
--- my-app/components/labeled-field.tsx
+++ my-app/components/labeled-field.tsx
@@ -3,13 +3,19 @@ import { Text, TextInput, View } from "react-native";
 
 interface LabeledFieldProps {
   label: string;
+  value: string;
+  onValueChange: (newValue: string) => void;
 }
 
 export function LabeledField(props: LabeledFieldProps) {
   return (
     <View style={{ gap: 8 }}>
       <Text>{props.label}</Text>
-      <TextInput style={{ backgroundColor: "#FFF" }} />
+      <TextInput
+        onChangeText={props.onValueChange}
+        value={props.value}
+        style={{ backgroundColor: "#FFF" }}
+      />
     </View>
   );
 }
