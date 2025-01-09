0dcd15ed7ee9069c3e5cbd295f8047faca9f77b4
BaraldsBR <gabriel.baraldi@taqtile.com.br>
2025-01-09 16:02:35 -0300

    add login handling

--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -1,5 +1,6 @@
 import { login } from "@/api/user/login";
 import { LabeledField } from "@/components/labeled-field";
+import { storeData } from "@/utils/storage";
 import { validateEmail } from "@/utils/validate-email";
 import { validatePassword } from "@/utils/validate-password";
 import * as React from "react";
@@ -22,6 +23,8 @@ export default function Index() {
     setPassword(newPassword);
   }
 
+  const [loginError, setLoginError] = React.useState("");
+
   function handleSubmit() {
     const emailValidationResult = validateEmail(email);
 
@@ -33,9 +36,16 @@ export default function Index() {
     setValidPassword(passwordValidationResult.valid);
     setPasswordErrorMessage(passwordValidationResult.errorMessage ?? "");
 
-    login(email, password).then((response) => {
-      console.log(response);
-    });
+    if (emailValidationResult.valid && passwordValidationResult.valid) {
+      login(email, password).then((response) => {
+        if (response.data) {
+          storeData("token", response.data.token);
+          setLoginError("");
+        } else {
+          setLoginError(response.errors?.[0].message ?? "");
+        }
+      });
+    }
   }
 
   return (
@@ -72,6 +82,7 @@ export default function Index() {
       >
         <Text>Entrar</Text>
       </Pressable>
+      {loginError && <Text>{loginError}</Text>}
     </View>
   );
 }
--- my-app/interfaces/apiResponse.ts
+++ my-app/interfaces/apiResponse.ts
@@ -2,5 +2,5 @@ import { ApiError } from "./apiError";
 
 export interface ApiResponse<T> {
   data: T | null;
-  errorrs?: ApiError[];
+  errors?: ApiError[];
 }
