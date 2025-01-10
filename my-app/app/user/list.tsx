26026a3693c8973c995cd5a32720290a426ea0d1
BaraldsBR <gabriel.baraldi@taqtile.com.br>
2025-01-10 12:21:53 -0300

    user list page

--- my-app/app/user/list.tsx
+++ my-app/app/user/list.tsx
@@ -1,5 +1,21 @@
+import { UserInfoCard } from "@/components/user-info-card";
 import * as React from "react";
-import { View, Text } from "react-native";
+import { View, FlatList } from "react-native";
+
+const userList = [
+  {
+    name: "Exemplo 1",
+    email: "exemplo1@exemplo.com",
+  },
+  {
+    name: "Exemplo 2",
+    email: "exemplo2@exemplo.com",
+  },
+  {
+    name: "Exemplo 3",
+    email: "exemplo3@exemplo.com",
+  },
+];
 
 export default function UserList() {
   return (
@@ -7,10 +23,15 @@ export default function UserList() {
       style={{
         flex: 1,
         padding: 16,
-        gap: 8,
       }}
     >
-      <Text>Olá :p</Text>
+      <FlatList
+        renderItem={(data) => {
+          return <UserInfoCard name={data.item.name} email={data.item.email} />;
+        }}
+        contentContainerStyle={{ gap: 8 }}
+        data={userList}
+      />
     </View>
   );
 }
