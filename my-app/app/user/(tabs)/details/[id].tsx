9a69cce0034ef623d95d573fbe258635f0bdd76a
BaraldsBR <gabriel.baraldi@taqtile.com.br>
2025-01-15 11:13:06 -0300

    add loading and error handling

--- my-app/app/user/(tabs)/details/[id].tsx
+++ my-app/app/user/(tabs)/details/[id].tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { View, Text, ActivityIndicator } from "react-native";
 import { User } from "@/interfaces/user";
 import { roleEnToPt } from "@/utils";
 import { detailUser } from "@/api/user/detail";
@@ -7,46 +7,70 @@ import { useLocalSearchParams } from "expo-router";
 
 export default function UserDetails() {
   const { id } = useLocalSearchParams<{ id: string }>();
+
+  const [loading, setLoading] = React.useState(false);
+  const [errorMessage, setErrorMessage] = React.useState<string>();
   const [user, setUser] = React.useState<User>();
 
   React.useEffect(() => {
     async function fetchUserDetails() {
+      setLoading(true);
       const response = await detailUser(id);
+      setLoading(false);
       if (response.data) {
         setUser(response.data);
+        setErrorMessage(undefined);
+      } else {
+        setErrorMessage(response.errors?.[0].message);
       }
     }
 
     fetchUserDetails();
   }, [id]);
 
-  return (
+  return loading ? (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <ActivityIndicator size={"large"} />
+    </View>
+  ) : (
     <View
       style={{
         flex: 1,
         padding: 16,
       }}
     >
-      <Text>
-        <Text style={{ fontWeight: "bold" }}>Nome Completo:</Text>{" "}
-        {user?.name ?? "Não Informado"}
-      </Text>
-      <Text>
-        <Text style={{ fontWeight: "bold" }}>E-Mail:</Text>{" "}
-        {user?.email ?? "Não Informado"}
-      </Text>
-      <Text>
-        <Text style={{ fontWeight: "bold" }}>Telefone:</Text>{" "}
-        {user?.phone ?? "Não Informado"}
-      </Text>
-      <Text>
-        <Text style={{ fontWeight: "bold" }}>Data de nascimento:</Text>{" "}
-        {user?.birthDate ?? "Não Informado"}
-      </Text>
-      <Text>
-        <Text style={{ fontWeight: "bold" }}>Nível de perissão:</Text>{" "}
-        {user?.role ? roleEnToPt(user.role) : "Não Informado"}
-      </Text>
+      {errorMessage ? (
+        <Text>{errorMessage}</Text>
+      ) : (
+        <>
+          <Text>
+            <Text style={{ fontWeight: "bold" }}>Nome Completo:</Text>{" "}
+            {user?.name ?? "Não Informado"}
+          </Text>
+          <Text>
+            <Text style={{ fontWeight: "bold" }}>E-Mail:</Text>{" "}
+            {user?.email ?? "Não Informado"}
+          </Text>
+          <Text>
+            <Text style={{ fontWeight: "bold" }}>Telefone:</Text>{" "}
+            {user?.phone ?? "Não Informado"}
+          </Text>
+          <Text>
+            <Text style={{ fontWeight: "bold" }}>Data de nascimento:</Text>{" "}
+            {user?.birthDate ?? "Não Informado"}
+          </Text>
+          <Text>
+            <Text style={{ fontWeight: "bold" }}>Nível de perissão:</Text>{" "}
+            {user?.role ? roleEnToPt(user.role) : "Não Informado"}
+          </Text>
+        </>
+      )}
     </View>
   );
 }
