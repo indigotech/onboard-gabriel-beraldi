815da8ec66f71ca56748036b62e598967b985820
BaraldsBR <gabriel.baraldi@taqtile.com.br>
2025-01-17 09:55:02 -0300

    drop .catch

--- my-app/api/user/list-users.ts
+++ my-app/api/user/list-users.ts
@@ -3,19 +3,23 @@ import { ApiResponse } from "@/interfaces/api-response";
 import { Pagination } from "@/interfaces/pagination";
 import { PaginatedRequest } from "@/interfaces/paginated-request";
 import { apiClient } from "@/api";
-import { AxiosError, isAxiosError } from "axios";
+import { isAxiosError } from "axios";
 
 export async function listUsers(
   params?: PaginatedRequest,
 ): Promise<ApiResponse<Pagination<User>>> {
-  const response = await apiClient
-    .get<ApiResponse<Pagination<User>>>("/users", { params })
-    .catch((error: Error | AxiosError) => {
-      if (isAxiosError(error)) {
-        return error.response;
-      } else {
-        throw error;
-      }
-    });
-  return response?.data;
+  try {
+    const response = await apiClient.get<ApiResponse<Pagination<User>>>(
+      "/users",
+      { params },
+    );
+
+    return response.data;
+  } catch (error) {
+    if (isAxiosError(error)) {
+      return error.response?.data;
+    } else {
+      throw error;
+    }
+  }
 }
